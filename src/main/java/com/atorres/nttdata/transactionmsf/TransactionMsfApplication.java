990b64d72477fc0cd9802b5af1603fb6ba60019d
AlejandroTorresUNSMS <alephdmc2575@gmail.com>
2023-07-29 11:23:05 -0500

    dev: commit inicial

---
+++ src/main/java/com/atorres/nttdata/transactionmsf/TransactionMsfApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.transactionmsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionMsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionMsfApplication.class, args);
+	}
+
+}
